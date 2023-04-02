90ad48cf6cb120c7c5194ac9721f123e1a1380de
Yandhi <15877410869@163.com>
2023-04-03 00:32:56 +0800

    参数错误捕获，错误信息提示

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -2,10 +2,28 @@
 
 const arg = require('arg');
 
-const args = arg({
-  '--start': Boolean,
-  '--build': Boolean,
-  '--test': Boolean,
-})
+try {
+  const args = arg({
+    '--start': Boolean,
+    '--build': Boolean,
+    '--test': String,
+  })
+  if (args['--start']) {
+    console.log('starting the app');
+  } else if (args['--build']) {
+    console.log('building the app');
+  } else if (args['--test']) {
+    console.log('testing the app', args['--test']);
+  }
+} catch (error) {
+  console.log(error.message);
+  console.log()
+  usage()
+}
 
-console.log(args);
+function usage() {
+  console.log(`tool [CMD]
+  --start(Boolean)\tStarts the app
+  --build(Boolean)\tBuilds the app
+  --test(String)\tTests the app`);
+}
