51329a7b0008460dd428d37aac54f98560008300
Yandhi <15877410869@163.com>
2023-04-13 00:05:36 +0800

    createLogger

---
+++ tool/src/logger.js
@@ -0,0 +1,10 @@
+import chalk from "chalk";
+
+module.export = function createLogger(name) {
+  return {
+    log: (...args) => console.log(chalk.gray(...args)),
+    warning: (...args) => console.log(chalk.yellow(...args)),
+    highlight: (...args) => console.log(chalk.bgCyanBright(...args)),
+    debug: debug(name)
+  };
+}          
